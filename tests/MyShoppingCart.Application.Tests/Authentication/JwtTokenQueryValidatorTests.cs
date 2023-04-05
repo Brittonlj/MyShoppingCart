dec60945065f7145af89a9f2bd827b682bdf92e3
Britt Johnson <brittonlj@gmail.com>
2023-04-05 04:37:38 -0500

    started Application tests

---
+++ tests/MyShoppingCart.Application.Tests/Authentication/JwtTokenQueryValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using MyShoppingCart.Application.Authentication;
+
+namespace MyShoppingCart.Application.Tests.Authentication;
+
+public class JwtTokenQueryValidatorTests
+{
+    private readonly IValidator<JwtTokenQuery> _validator = new JwtTokenQueryValidator();
+
+    [Fact]
+    public async Task Validate_ShouldReturnNoResults_WhenStateIsValid()
+    {
+        //Arrange
+        var jwtTokenQuery = new JwtTokenQuery(Guid.NewGuid());
+
+        //Act
+        var results = await _validator.ValidateAsync(jwtTokenQuery);
+
+        //Assert
+        results.Should().NotBeNull();
+        results.Errors.Should().NotBeNull().And.BeEmpty();
+        results.RuleSetsExecuted.Should().NotBeNull().And.NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Validate_ShouldReturnResults_WhenCustomerIdIsEmpty()
+    {
+        //Arrange
+        var jwtTokenQuery = new JwtTokenQuery(Guid.Empty);
+
+        //Act
+        var results = await _validator.ValidateAsync(jwtTokenQuery);
+
+        //Assert
+        results.Should().NotBeNull();
+        results.Errors.Should().NotBeNull().And.NotBeEmpty();
+        results.Errors.First().PropertyName.Should().Be(nameof(JwtTokenQuery.CustomerId));
+        results.Errors.First().ErrorMessage.Should().Be("'Customer Id' must not be empty.");
+        results.RuleSetsExecuted.Should().NotBeNull().And.NotBeEmpty();
+    }
+}
---
+++ tests/MyShoppingCart.Application.Tests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Xunit;
+global using Moq;
+global using FluentAssertions;
+global using FluentValidation;
+global using MyShoppingCart.Application.Customers;
+global using MyShoppingCart.Domain.Models;
